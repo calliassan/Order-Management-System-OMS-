2d7f37ee01fb0677cec0c9efcef83c957e84491b
Ashish Bhattarai <ashishbhattarai49@gmail.com>
2026-04-15 15:51:00 +0530

    added empty handling

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use("/api", orderRoutes);
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Server running on port ${process.env.PORT}`),

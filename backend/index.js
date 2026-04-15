200317479a6c3ec77d2cbfa45e0955ba7577d07d
Ashish Bhattarai <ashishbhattarai49@gmail.com>
2026-04-15 15:54:02 +0530

    console changed

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,23 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 const orderRoutes = require("./routes/orderRoutes");
-const cors = require("cors");
+
 app.use(cors());
 app.use(express.json());
+
 app.use("/api", orderRoutes);
+
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`Server running on port ${process.env.PORT}`),
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
